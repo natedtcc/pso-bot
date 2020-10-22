04bf0f63ddedc095f493ce2296c19f52f2e7b4ea
Nate Nasteff <nnasteff@protonmail.com>
2020-10-22 00:16:54 -0400

    updated to reflect current class struct

--- src/test/WarpTester.java
+++ src/test/WarpTester.java
@@ -7,7 +7,7 @@ public class WarpTester {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		WarpBot memtest = new WarpBot(30, 19);
+		WarpBot memtest = new WarpBot(1);
 	
 		memtest.start();
 	}
